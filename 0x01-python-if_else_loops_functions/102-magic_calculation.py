fb6b1144ddfbd4abe319f6205b735bdba5468928
DesmondOdhiambo <desmodhiambo@gmail.com>
2023-01-16 08:17:56 -0800

    Commit Message

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    if c > b:
+        return a + b
+    return (a * b) - c
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+
